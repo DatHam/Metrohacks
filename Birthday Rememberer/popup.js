d427149c5cd8f170a9a42270bd3b73e07f0e1d63
DatHam <alan.zhou.0@gmail.com>
2021-11-21 14:58:23 -0600

    added "add birthday" button

--- Birthday Rememberer/popup.js
+++ Birthday Rememberer/popup.js
@@ -1,19 +1,3 @@
-function handleButtonClick(){
-    // Use stored sync value.
-    chrome.storage.sync.get("textBoxValue", ({ textBoxValue }) => {
-       alert(textBoxValue);
-    });
- }
- 
- document.getElementById("popupButton").addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    
-    // Store sync value before the script is executed.
-    let textBoxValue = document.getElementById('textBox').value;
-    chrome.storage.sync.set({ textBoxValue });
- 
-    chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        function: handleButtonClick,
-    });
- });
+var addBirthdayButton = document.createElement("button");
+addBirthdayButton.innerHTML = "Add Birthday";
+document.body.appendChild(addBirthdayButton);
