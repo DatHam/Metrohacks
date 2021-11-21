abe52772c16256ba37e23d9d47d3700e5dd0fea8
DatHam <alan.zhou.0@gmail.com>
2021-11-21 17:43:40 -0600

    java first test

---
+++ SortBirthdays.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.*;
+public class SortBirthdays {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner("birthdayList.txt");
+        
+    }
+}
